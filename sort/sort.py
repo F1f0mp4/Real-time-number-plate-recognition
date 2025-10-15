0205e54ddd215908d76040dc3bf1799aa8b73096
Filip <77928490+F1f0mp4@users.noreply.github.com>
2025-10-15 20:15:41 +0200

    1st push

---
+++ sort/sort.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Sort:
+    def __init__(self):
+        self.tracks = []
+
+    def update(self, detections):
+        # Simple passthrough for demo
+        return np.array(detections)
